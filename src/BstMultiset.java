68b6a1dc5d083a2fbccd7e00ece14102a051b4ff
russellmenezes <s3600950@student.rmit.edu.au>
2017-08-12 20:05:03 +1000

    added remove all but not working

--- src/BstMultiset.java
+++ src/BstMultiset.java
@@ -52,7 +52,10 @@ public class BstMultiset<T> extends Multiset<T>
 		BstNode<T> node = null;
 		
 		//find the node
-		node = find(item, head);
+		if(head.data != null)
+		{
+			node = find(item, head);
+		}
 		
 		//if the node is found then remove one from counter
 		if(node != null)
@@ -72,6 +75,107 @@ public class BstMultiset<T> extends Multiset<T>
 	
 	public void removeAll(T item) {
 		// Implement me!
+		BstNode<T> node = null;
+		BstNode<T> pre = null;
+		boolean left = false;
+		boolean h = false;
+		
+		//find the node
+		if(head.data != null)
+		{
+			node = find(item, head);
+			pre = findPre(item, head);
+		}
+		
+		if(node == null)
+		{
+			return;
+		}
+		
+		if(pre == null)
+		{
+			pre = head;
+			h = true;
+		}
+		else if(pre.next != null && pre.next.equals(node))
+		{
+			left = true;
+		}
+		else if(pre.previous != null && pre.previous.equals(node))
+		{
+			left = false;
+		}
+		else
+		{
+			return;
+		}
+		
+		if(node.previous == null && node.next == null)
+		{
+			if(h)
+			{
+				pre = null;
+			}
+			else if(left)
+			{
+				pre.previous = null;
+			}
+			else
+			{
+				pre.next = null;
+			}
+		}
+		else if(node.previous != null && node.next == null)
+		{
+			if(h)
+			{
+				pre = node.previous;
+			}
+			else if(left)
+			{
+				pre.previous = node.previous;
+			}
+			else
+			{
+				pre.next = node.previous;
+			}
+		}
+		else if(node.previous == null && node.next != null)
+		{
+			if(h)
+			{
+				pre = node.next;
+			}
+			else if(left)
+			{
+				pre.previous = node.next;
+			}
+			else
+			{
+				pre.next = node.next;
+			}
+		}
+		else
+		{
+			
+			BstNode<T> end = findLast(node.previous);
+			
+			if(h)
+			{
+				pre = node.previous;
+			}
+			if(left)
+			{
+				pre.previous = node.previous;
+			}
+			else
+			{
+				pre.next = node.previous;
+			}
+			
+			end.next = node.next;
+		}
+		
 	} // end of removeAll()
 
 
@@ -93,7 +197,7 @@ public class BstMultiset<T> extends Multiset<T>
 		}
 		else
 		{
-			if((long) data > (long) current.data) //greater than, insert on the right if empty
+			if( Long.parseLong((String) data) > Long.parseLong((String) current.data)) //greater than, insert on the right if empty
 			{
 				if(current.next == null)
 				{
@@ -126,7 +230,7 @@ public class BstMultiset<T> extends Multiset<T>
 		}
 		else
 		{
-			if((long) data > (long) current.data) //greater than, find on the right
+			if(Long.parseLong((String) data) > Long.parseLong((String) current.data)) //greater than, find on the right
 			{
 				if(current.next == null) // if right is empty then return null
 				{
@@ -151,10 +255,66 @@ public class BstMultiset<T> extends Multiset<T>
 		}
 	}
 	
+	public BstNode<T> findPre(T data, BstNode<T> current)
+	{
+		if(data.equals(current.data)) //check if this node is the one matching data and return that node
+		{
+			return null;
+		}
+		else
+		{
+			if(Long.parseLong((String) data) > Long.parseLong((String) current.data)) //greater than, find on the right
+			{
+				if(current.next == null) // if right is empty then return null
+				{
+					return null;
+				}
+				else //if there is data on the right the keep looking on the right
+				{
+					if(data.equals(current.next.data)) //check if this node is the one matching data and return that node
+					{
+						return current;
+					}
+					
+					return findPre(data, current.next);
+				}
+			}
+			else //less than, find on the left
+			{
+				if(current.previous == null) //if the left is empty then return null
+				{
+					return null;
+				}
+				else //if there is data on the left then keep looking on the left
+				{
+					if(data.equals(current.previous.data)) //check if this node is the one matching data and return that node
+					{
+						return current;
+					}
+					
+					return findPre(data, current.previous);
+					
+				}
+			}
+		}
+	}
+	
+	public BstNode<T> findLast(BstNode<T> node)
+	{
+		if(node.next == null)
+		{
+			return node;
+		}
+		else
+		{
+			return findLast(node.next);
+		}
+	}
+	
 	public void printEach(BstNode<T> current, PrintStream out)
 	{
 		//print out the current set of data
-		out.println(current.data + " | " + current.data);
+		out.println(current.data + " | " + current.count);
 		
 		//check if the right side of the tree exists
 		if(current.next != null)
